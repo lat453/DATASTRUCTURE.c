92169b0d4274bba62acb1b2f6263d7e1abbe967e
lat453 <151341442+lat453@users.noreply.github.com>
2024-11-10 21:17:33 +0530

    Create Scenario-6.c

---
+++ EX-2/Scenario-6.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    char data;
+    struct Node* next;
+};
+
+struct Node* createNode(char data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void push(struct Node** head, char data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+char pop(struct Node** head) {
+    if (*head == NULL) return '\0';
+    struct Node* temp = *head;
+    char data = temp->data;
+    *head = (*head)->next;
+    free(temp);
+    return data;
+}
+
+int isEmpty(struct Node* head) {
+    return head == NULL;
+}
+
+int isMatchingPair(char open, char close) {
+    return (open == '(' && close == ')') || (open == '{' && close == '}') || (open == '[' && close == ']');
+}
+
+int isBalanced(char* expression) {
+    struct Node* stack = NULL;
+    for (int i = 0; expression[i] != '\0'; i++) {
+        if (expression[i] == '(' || expression[i] == '{' || expression[i] == '[')
+            push(&stack, expression[i]);
+        else if (expression[i] == ')' || expression[i] == '}' || expression[i] == ']') {
+            if (isEmpty(stack) || !isMatchingPair(pop(&stack), expression[i]))
+                return 0;
+        }
+    }
+    return isEmpty(stack);
+}
+
+int main() {
+    char expression[100];
+    printf("Enter an expression: ");
+    scanf("%s", expression);
+    if (isBalanced(expression))
+        printf("Balanced\n");
+    else
+        printf("Not Balanced\n");
+    return 0;
+}
